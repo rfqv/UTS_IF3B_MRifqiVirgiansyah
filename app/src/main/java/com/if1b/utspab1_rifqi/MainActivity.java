16ebd591a069274fdc22c2cd226581e8e6d549f0
friska <rifqivirgiansyah@gmail.com>
2022-11-02 15:10:25 +0700

    update2

--- app/src/main/java/com/if1b/utspab1_rifqi/MainActivity.java
+++ app/src/main/java/com/if1b/utspab1_rifqi/MainActivity.java
@@ -5,11 +5,38 @@ import android.os.Bundle;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
-    String[] tipejalurdaftar = { "Tes Tertulis", "Jalur Tanpa Tes", "Ujian Saringan Masuk"};
+    String[] tipejalurdaftar = { "Tes Tertulis", "Jalur Tanpa Tes", "Ujian Saringan Masuk"}, nomorDaftar, nama;
+    EditText etNmrDaftar, etNama;
+    Button btnDaftar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Layout A");
+        btnDaftar = findViewById(R.id.btn_daftar);
+        etNama = findViewById(R.id.et_nama);
+
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nama = etNama.getText().toString();
+                if (nama.trim().equals("")) {
+                    etNama.setError("Nama Harus Diisi!");
+                } else {
+                    Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
+                    pindah.putExtra("xNama", nama);
+                    startActivity(pindah);
+                }
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +45,18 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         Spinner spinaja = findViewById(R.id.sp_jalurdaftar);
         spinaja.setOnItemSelectedListener(this);
 
-        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item,tipejalurdaftar);
+        ArrayAdapter<String> ad = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,tipejalurdaftar);
         ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinaja.setAdapter(ad);
     }
     @Override
-    public void OnItemSelectedListener(AdapterView<*> arg0, View arg1, int position, long id)
-    {
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id){
         Toast.makeText(getApplicationContext(),tipejalurdaftar[position], Toast.LENGTH_LONG).show();
     }
     @Override
-    public void onNothingSelected(AdapterView<*> arg0)
-    {
+    public void onNothingSelected(AdapterView<?> arg0){
         // Auto-generated method stub
     }
+
+
 }
