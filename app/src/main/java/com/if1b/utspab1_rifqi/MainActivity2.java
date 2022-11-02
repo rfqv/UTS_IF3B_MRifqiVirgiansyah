d201987dcfc1f22e9131e50e2c797aa6a6236d5d
friska <>
2022-11-02 14:05:27 +0700

    update1

---
+++ app/src/main/java/com/if1b/utspab1_rifqi/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.if1b.utspab1_rifqi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
